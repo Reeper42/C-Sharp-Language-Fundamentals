dee59798d4f01d7c55470ca0529fb66cc7ca0ab1
Daniel Reep <dan.reep@yahoo.com>
2020-02-15 14:20:50 -0600

    Adding DogGenetics to BasicProgrammingConcepts

---
+++ BasicProgrammingConcepts/DogGenetics/DogGenetics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogGenetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userDog;
+
+
+            Console.WriteLine("What is your dog's name?  ");
+            userDog = Console.ReadLine();
+
+            Console.WriteLine("Well then, I have this highly reliable report on " + userDog + "prestigious background right here.");
+        }
+    }
+}
